48328240e1c0bcc9e0f20d7b5e65072f3bc60a19
Jiri Dohnalek <thinkabout3e@googlemail.com>
2017-07-01 20:06:40 +0100

    Update and rename email2pi.py to gmail2pi.py

--- gmail2pi.py
+++ gmail2pi.py
@@ -1,23 +1,34 @@
-#imports for thermometer reading
-import os
-import glob
+# IMPORTS #########################################################
+
+
 import time
-#imports for gmail reading
 import imaplib
 import email
-
 import smtplib
-
 import hashlib
 import string
 import random
 
 
+# CONSTANTS #######################################################
+
+
+# List of allowed email addresses that can send email to the 
+# Raspaberry Pi
 ALLOWED = []
 
-EMAIL = 'email here'
+# Email address used by Raspberry Pi to probe for email with
+# a specific subject to trigger action
+EMAIL = 'email@example.com'
+
+# Password to access the email address
 PASSWORD = 'password'
-SUBJECT = 'subject'
+
+# Triggering subject (Do not change)
+SUBJECT = '(rfc822)'
+
+
+# FUNCTIONS ######################################################
 
 
 def id_generator(size=15, chars=string.ascii_uppercase + string.digits):
@@ -36,10 +47,10 @@ def generate_code():
     return m.hexdigest()
 
 
-def send_gmail(to, subject, body):
+def send_gmail(to, subject, code):
     """
     Send email
-    
+
     :param to: send the email to
     :param subject: email subject
     :param body: the text of the message
@@ -47,8 +58,8 @@ def send_gmail(to, subject, body):
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
     smtpObj.ehlo()
     smtpObj.starttls()
-    smtpObj.login(ADDR, PASS)
-    smtpObj.sendmail(ADDR, to, 'Subject: ' + subject + '\n' + body)
+    smtpObj.login(EMAIL, PASSWORD)
+    smtpObj.sendmail(EMAIL, to, 'Subject: ' + subject + '\n' + code)
 
 
 def read_gmail(subject):
@@ -61,9 +72,10 @@ def read_gmail(subject):
 
     varSubject = None
     varFrom = None
+    varBody = None
 
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
-    mail.login(ADDR, PASS)
+    mail.login(EMAIL, PASSWORD)
     mail.select('inbox')
 
     status, msgs = mail.select('inbox')
@@ -91,8 +103,18 @@ def read_gmail(subject):
             if isinstance(response_part, tuple):
                 msg = email.message_from_string(response_part[1])
 
+        # get the body of the message
+        if msg.is_multipart():
+            for payload in msg.get_payload():
+                varBody += payload.get_payload()
+        else:
+            varBody = msg.get_payload()
+
+        # get the subject of the message
         varSubject = msg['subject']
         varFrom = msg['from']
+
+        # get from who is the message
         varFrom = varFrom.replace('<','')
         varFrom = varFrom.replace('>','')
 
@@ -115,14 +137,13 @@ def read_gmail(subject):
     if count is 0:
         return False
 
+    varFrom = varFrom.split()[len(varFrom.split())-1]
 
-    message = varSubject.replace(subject, '')
-    message = message.replace('Re:','')
-    message = message.strip()
+    # Strip the uneccesary text fro the body
+    if "\n" in varBody:
+        varBody = varBody.split("\n")[0]
 
-    sender = varFrom.split()[len(varFrom.split())-1]
-
-    return { 'sender': sender, 'message' : message }
+    return { 'sender': varFrom, 'subject' : varSubject, 'body': varBody }
 
 
 def main():
@@ -145,7 +166,7 @@ def main():
             secret_key = generate_code()
             print "Generated Code: " + secret_key
             original_sender = first_reply['sender']
-            send_gmail(original_sender, SUBJECT, secret_key)
+            send_gmail(original_sender, SUBJECT + " " + secret_key, "")
             print "Reply sent to " + first_reply['sender']
             print "Waiting for reply"
 
@@ -175,9 +196,9 @@ def main():
                     print "Email received from " + second_reply['sender']
                     print "Checking key"
 
-                    if second_reply['sender'] == original_sender and second_reply['message'].find(secret_key) is not -1:
+                    if second_reply['sender'] == original_sender and second_reply['subject'].find(secret_key) is not -1:
 
-                        command = second_reply['message'].replace(secret_key, '')
+                        command = second_reply['body']
                         print "Secret key matched"
                         print "Command to execute:" + command.strip()
                         secret_key = None # destroy the key
@@ -192,12 +213,17 @@ def main():
                         print "Generating new key"
                         secret_key = generate_code()
                         print "Re-generated code: " + secret_key
-                        send_gmail(original_sender, SUBJECT, "Old key failed, new key is " + secret_key)
+                        send_gmail(original_sender, SUBJECT + " [NEW] " + secret_key, "")
                         print "New key sent to " + original_sender
 
                 time.sleep(5)
                 timecount += 5
 
-
-
         time.sleep(5)
+
+
+#########################
+
+
+if __name__ == '__main__':
+    main()
