108b95bd9a4dcea7a577363670a7c024494b6fbb
Jiri Dohnalek <thinkabout3e@googlemail.com>
2017-09-21 09:31:46 +0100

    Improved error handling

--- gmail2pi.py
+++ gmail2pi.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
 """
 Original forked from https://github.com/joolswhitehorn/stuff-projects
 
-Communicate with Raspberry Pi or any computer via Google Mail 
+Communicate with Raspberry Pi or any computer via Google Mail
 Copyright (C) 2017  Jiri Dohnalek
 
 This program is free software; you can redistribute it and/or modify
@@ -19,9 +20,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-
 # IMPORTS #########################################################
-
 
 import time
 import imaplib
@@ -30,25 +29,30 @@ import smtplib
 import hashlib
 import string
 import random
-
+import requests
 
 # CONSTANTS #######################################################
 
-
-# List of allowed email addresses that can send email to the 
+# List of allowed email addresses that can send email to the
 # Raspaberry Pi
 ALLOWED = []
 
 # Email address used by Raspberry Pi to probe for email with
 # a specific subject to trigger action
-EMAIL = 'email@example.com'
+EMAIL = ''
 
 # Password to access the email address
-PASSWORD = 'password'
+PASSWORD = ''
 
 # Triggering subject (Do not change)
 SUBJECT = '(rfc822)'
 
+
+# Time after which the secret key expires
+TIMEOUT = 5 * 60
+
+# How often to check email in second stage
+INCREMENTS = 5
 
 # FUNCTIONS ######################################################
 
@@ -59,17 +63,15 @@ def id_generator(size=15, chars=string.ascii_uppercase + string.digits):
     """
     return ''.join(random.choice(chars) for _ in range(size))
 
-
 def generate_code():
     """
     Convert the string using md5 to create the exchange key
     """
     m = hashlib.md5()
-    m.update( str(random.randint(0,9999)) + id_generator() + str(random.randint(0,9999)) )
+    m.update(str(random.randint(0, 9999)) + id_generator() + str(random.randint(0, 9999)))
     return m.hexdigest()
 
-
-def send_gmail(to, subject, code):
+def send_gmail(to, subject, body):
     """
     Send email
 
@@ -81,8 +83,7 @@ def send_gmail(to, subject, code):
     smtpObj.ehlo()
     smtpObj.starttls()
     smtpObj.login(EMAIL, PASSWORD)
-    smtpObj.sendmail(EMAIL, to, 'Subject: ' + subject + '\n' + code)
-
+    smtpObj.sendmail(EMAIL, to, 'Subject: ' + subject + '\n' + body)
 
 def read_gmail(subject):
     """
@@ -92,19 +93,31 @@ def read_gmail(subject):
              else false
     """
 
-    varSubject = None
-    varFrom = None
-    varBody = None
+    subject = None
+    sender = None
+    body = ""
 
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     mail.login(EMAIL, PASSWORD)
-    mail.select('inbox')
-
-    status, msgs = mail.select('inbox')
-    if status != 'OK':
-        print "Can not connect"
-        return
-
+    
+    try:
+        mail.select('inbox')
+    except imaplib.IMAP4.abort:
+        return False
+    except imaplib.IMAP4.error
+        return False
+    
+    try:
+        status, msgs = mail.select('inbox')
+        if status != 'OK':
+            print "Can not connect"
+            return False
+    
+    except imaplib.IMAP4.abort:
+        return False
+    except imaplib.IMAP4.error
+        return False
+        
     mail.list()
 
     typ, data = mail.search(None, 'ALL')
@@ -116,10 +129,10 @@ def read_gmail(subject):
 
     if len(id_list) > 0:
 
-        latest_email_id = int( id_list[-1] )
+        latest_email_id = int(id_list[-1])
 
-        for i in range( latest_email_id, latest_email_id-1, -1):
-            typ, data = mail.fetch( i, subject)
+        for i in range(latest_email_id, latest_email_id - 1, -1):
+            typ, data = mail.fetch(i, subject)
 
         for response_part in data:
             if isinstance(response_part, tuple):
@@ -128,19 +141,22 @@ def read_gmail(subject):
         # get the body of the message
         if msg.is_multipart():
             for payload in msg.get_payload():
-                varBody += payload.get_payload()
+                try:
+                    body += payload.get_payload()
+                except TypeError:
+                    body += ""
         else:
-            varBody = msg.get_payload()
+            body = msg.get_payload()
 
         # get the subject of the message
-        varSubject = msg['subject']
-        varFrom = msg['from']
+        subject = msg['subject']
+        sender = msg['from']
 
         # get from who is the message
-        varFrom = varFrom.replace('<','')
-        varFrom = varFrom.replace('>','')
+        sender = sender.replace('<', '')
+        sender = sender.replace('>', '')
 
-        #Remove used emails from mailbox
+        # Remove used emails from mailbox
         typ, data = mail.search(None, 'ALL')
 
     for num in data[0].split():
@@ -149,103 +165,121 @@ def read_gmail(subject):
         mail.close()
         mail.logout()
 
-    if varSubject is None or varFrom is None:
+    if subject is None or sender is None:
         return False
 
     count = 0
     for _email in ALLOWED:
-        if varFrom.find(_email) is not -1:
+        if sender.find(_email) is not -1:
             count += 1
     if count is 0:
         return False
 
-    varFrom = varFrom.split()[len(varFrom.split())-1]
+    sender = sender.split()[len(sender.split()) - 1]
 
     # Strip the uneccesary text fro the body
-    if "\n" in varBody:
-        varBody = varBody.split("\n")[0]
+    if "\n" in body:
+        body = body.split("\n")[0]
 
-    return { 'sender': varFrom, 'subject' : varSubject, 'body': varBody }
-
+    return {'sender': sender, 'subject': subject, 'body': body}
 
 def main():
-
+    """ Main routine """
     print "Waiting for an email"
 
     while True:
 
-        secret_key = None;
+        while True:
 
-        # The first phase email
-        first_reply = read_gmail(SUBJECT)
-        if first_reply is False:
+            secret_key = None
+            first_reply = False
+            second_reply = False
 
-            pass
+            # The first phase email
+            first_reply = read_gmail(SUBJECT)
+            # print "first reply:", first_reply
 
-        else:
+            if first_reply is False:
+                pass
 
-            print "Email received"
-            secret_key = generate_code()
-            print "Generated Code: " + secret_key
-            original_sender = first_reply['sender']
-            send_gmail(original_sender, SUBJECT + " " + secret_key, "")
-            print "Reply sent to " + first_reply['sender']
-            print "Waiting for reply"
+            else:
 
-            # Allow reply within 5 minutes
-            timeout = 5 * 60
-            timecount = 0
+                print "Email received"
+                secret_key = generate_code()
+                print "Generated Code: " + secret_key
+                original_sender = first_reply['sender']
+                send_gmail(original_sender, SUBJECT + " " + secret_key, "")
+                print "Reply sent to " + first_reply['sender']
+                print "Waiting for reply"
 
-            while True:
+                # Allow reply within 5 minutes
+                timecount = 0
 
-                # Timeout the key in 5 minutes
-                if timecount == timeout:
+                break
 
-                    print "Timeout"
+            time.sleep(15)
+
+        while True:
+
+            # Timeout the key in 5 minutes
+            if timecount == TIMEOUT:
+
+                print "Timeout"
+                print "Destroying the key"
+                secret_key = None  # destroy the key
+
+                send_gmail(original_sender, "System Timeout", "You have run out of time, please start again")
+                break
+
+            # The second phase email
+            second_reply = read_gmail(SUBJECT)
+            # print "second reply:", second_reply
+
+            if second_reply is False:
+                pass
+
+            else:
+                print "Email received from " + second_reply['sender']
+                print "Checking key"
+
+                condition1 = second_reply['sender'] == original_sender
+                condition2 = second_reply['subject'].find(secret_key) is not -1
+
+                if condition1 and condition2:
+
+                    command = second_reply['body'].upper().strip()
+                    print "Secret key matched"
+                    print "Command to execute:" + command
+
+                    send_gmail(
+                        original_sender,
+                        second_reply['subject'],
+                        'Command "{}" executed succesfully'.format(
+                            command.strip()
+                        )
+                    )
+
+                    print "Confirmation sent to " + original_sender
+                    secret_key = None  # destroy the key
                     print "Destroying the key"
-                    secret_key = None # destroy the key
-
-                    send_gmail(original_sender, "System Timeout", "You have run out of time, please start again")
-                    break
-
-                # The second phase email
-                second_reply = read_gmail(SUBJECT)
-
-                if second_reply is False:
-
-                    pass
+                    print "Waiting for an email"
 
                 else:
 
-                    print "Email received from " + second_reply['sender']
-                    print "Checking key"
+                    print "Key did not match"
+                    print "Generating new key"
+                    secret_key = generate_code()
+                    print "New generated code: " + secret_key
+                    send_gmail(original_sender, SUBJECT + " [NEW] " + secret_key, "")
+                    print "New key sent to " + original_sender
 
-                    if second_reply['sender'] == original_sender and second_reply['subject'].find(secret_key) is not -1:
-
-                        command = second_reply['body']
-                        print "Secret key matched"
-                        print "Command to execute:" + command.strip()
-                        send_gmail(original_sender, second_reply['subject'], "Command executed succesfully")
-                        print "Confirmation sent to " + original_sender
-                        secret_key = None # destroy the key
-                        print "Destroying the key"
-                        print "Waiting for an email"
-
-                    else:
-
-                        print "Key did not match"
-                        print "Generating new key"
-                        secret_key = generate_code()
-                        print "New generated code: " + secret_key
-                        send_gmail(original_sender, SUBJECT + " [NEW] " + secret_key, "")
-                        print "New key sent to " + original_sender
-
-                time.sleep(5)
-                timecount += 5
+                break
+            
+            time.sleep(INCREMENTS)
+            timecount += INCREMENTS
 
 
-
-        time.sleep(5)
+###########################
 
 
 if __name__ == '__main__':
